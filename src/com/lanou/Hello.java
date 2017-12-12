12280c5a8cf0b1bf42e100a79848c1f04b0be532
yWinter <winter951209@163.com>
2017-12-12 16:11:42 +0800

    初始化代码 作者:杨钊 时间:2017.12.12

---
+++ src/com/lanou/Hello.java
@@ -0,0 +1,26 @@
+package com.lanou;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter printWriter = resp.getWriter();
+		
+		printWriter.write("test");
+		
+	}
+
+}
