c40d5ec7f752539614aa4ddbaa2058447b1215eb
yWinter <winter951209@163.com>
2017-12-12 16:19:17 +0800

    修改注释

--- src/com/lanou/Hello.java
+++ src/com/lanou/Hello.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// 类
 @WebServlet("/Hello")
 public class Hello extends HttpServlet {
 	
